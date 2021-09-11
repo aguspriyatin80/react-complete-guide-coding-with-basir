69c8adf6109864c50f990f8a65a9a29f3a05ce86
Agus Priyatin <aguspriyatin80>
2021-09-12 05:01:12 +0700

    react state & lifecycle

--- react-basic-app/src/App.js
+++ react-basic-app/src/App.js
@@ -1,27 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import Welcome from './Welcome';
+import Message from './Message';
+import Clock from './Clock';
 
 function App() {
-  function formatName(userName){
-    return userName.toUpperCase()
-  }
-  function getGreeting(username){
-    if(username){
-      return <h1>Hello {username}</h1>
-    } else {
-      return <h1>Hello Stranger</h1>
-    }
-  }
-  const name = "Agus"  
-  const logo = '/logo192.png'
-  const image = <img src={logo} alt="logo"/>
-  const element = <div>
-    <h1>Hello {formatName(name)}</h1>
-    <p>Good to see you</p>
-    {image}
+  return (
+    <div>
+      <Welcome name="Eka" />
+      <Message message="Testing messages" />
+      <Clock date={new Date()} />
     </div>
-  // return getGreeting('Eka')
-  return element
+  )
 }
-
 export default App;
