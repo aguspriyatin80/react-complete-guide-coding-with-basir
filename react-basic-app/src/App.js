09d984117a4c64a17f0ed5ea1fb092aed1ddac44
Agus Priyatin <aguspriyatin80>
2021-09-13 00:32:35 +0700

    render list

--- react-basic-app/src/App.js
+++ react-basic-app/src/App.js
@@ -5,15 +5,29 @@ import Message from './Message';
 import Clock from './Clock';
 import ToggleButton from './ToggleButton';
 import ToggleButtonClass from './ToggleButtonClass';
+import NumberList from './NumberList';
 
 function App() {
+  const numbers = [1, 2, 3, 4, "Test"]
+  const numbers2 = [1, 8, 20, 22];
+  const todos = [
+    { id: '1', name: "Coding", isDone: false },
+    { id: '2', name: "Eating", isDone: true },
+  ]
   return (
     <div>
       {/* <Welcome name="Eka" />
       <Message message="Testing messages" />
       <Clock date={new Date()} /> */}
       {/* <ToggleButton /> */}
-      <ToggleButtonClass />
+      {/* <ToggleButtonClass /> */}
+      <NumberList numbers={numbers} />
+      <NumberList numbers={numbers2} />
+      <ul>
+        {
+          todos.map((todo) => <li key={todo.id}>{todo.name} - {todo.isDone ? 'Done' : ''}</li>)
+        }
+      </ul>
     </div>
   )
 }
---
+++ react-basic-app/src/NumberList.js
@@ -0,0 +1,10 @@
+function NumberList(props) {
+
+    return (
+        <ul>
+            {props.numbers.map((number, index) => (<li key={index}>{number}</li>))}
+        </ul>
+    )
+}
+
+export default NumberList
