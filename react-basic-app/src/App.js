fa314b2aa281871bf7348e92f580e81d3369d59b
Agus Priyatin <aguspriyatin80>
2021-09-14 16:19:01 +0700

    conditional rendering

--- react-basic-app/src/App.js
+++ react-basic-app/src/App.js
@@ -6,14 +6,16 @@ import Clock from './Clock';
 import ToggleButton from './ToggleButton';
 import ToggleButtonClass from './ToggleButtonClass';
 import NumberList from './NumberList';
+import Greeting from './Greeting';
+import LoginControl from './LoginControl';
 
 function App() {
-  const numbers = [1, 2, 3, 4, "Test"]
-  const numbers2 = [1, 8, 20, 22];
-  const todos = [
-    { id: '1', name: "Coding", isDone: false },
-    { id: '2', name: "Eating", isDone: true },
-  ]
+  // const numbers = [1, 2, 3, 4, "Test"]
+  // const numbers2 = [1, 8, 20, 22];
+  // const todos = [
+  //   { id: '1', name: "Coding", isDone: false },
+  //   { id: '2', name: "Eating", isDone: true },
+  // ]
   return (
     <div>
       {/* <Welcome name="Eka" />
@@ -21,13 +23,15 @@ function App() {
       <Clock date={new Date()} /> */}
       {/* <ToggleButton /> */}
       {/* <ToggleButtonClass /> */}
-      <NumberList numbers={numbers} />
-      <NumberList numbers={numbers2} />
-      <ul>
+      {/* <NumberList numbers={numbers} />
+      <NumberList numbers={numbers2} /> */}
+
+      {/* <ul>
         {
           todos.map((todo) => <li key={todo.id}>{todo.name} - {todo.isDone ? 'Done' : ''}</li>)
         }
-      </ul>
+      </ul> */}
+      <LoginControl hasNewMessage credit={100} hasWarning />
     </div>
   )
 }
